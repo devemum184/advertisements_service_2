f15490fc91115ae7a76cbc035125db49d2b480c5
devemum184 <devemum184@gmail.com>
2026-04-10 18:41:43 +0500

    add JWT and password hashing dependencies

--- app/config.py
+++ app/config.py
@@ -4,3 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/advertisements")
+SECRET_KEY = os.getenv("SECRET_KEY", "secret-key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_HOURS = int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS", "48"))
